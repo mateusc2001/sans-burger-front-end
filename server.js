c6087fb11ad9f56f788d17cf1ee8556b499b0f69
mateusc2001 <mateusc2001@gmail.com>
2022-01-18 01:15:58 -0300

    Configuracao para deploy no heroku

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const nomeApp = 'sans-burger';
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) =>
-    res.sendFile(`${__dirname}/src/index.html`),
-);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
